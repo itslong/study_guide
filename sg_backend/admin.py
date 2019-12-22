084b5e7e5f0b361dffd3a25895602fd7ff40e12b
Long Liang <lliang@Earth-C-137.local>
2019-12-22 01:09:33 -0800

    minimal admin registered. Included db migrations for models.

--- sg_backend/admin.py
+++ sg_backend/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Categories, Topics, Notes
 
-# Register your models here.
+admin.site.register(Categories)
+admin.site.register(Topics)
+admin.site.register(Notes)
