a9ff1007b0eff750a1b835911b37e47149473a32
Long Liang <lliang@Earth-C-137.local>
2020-01-06 14:43:38 -0800

    Clarified View, Serializer names. Added more routes.

--- sg_backend/views.py
+++ sg_backend/views.py
@@ -1,31 +1,58 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
 
-from sg_backend.models import Categories
-from sg_backend.serializers import UsersListSerializer, UserDetailSerializer, UserDetailWithAllData, CategoriesListSerializer, UserCategoryDetails, CategoryWithTopicsSerializer
+from sg_backend.models import Categories, Topics, Notes
+from . import serializers
 
-
+# User based views
 class AllUsersListView(generics.ListAPIView):
-    serializer_class = UsersListSerializer
+    serializer_class = serializers.UsersListSerializer
     queryset = User.objects.all()
 
 
 class UserDetailsView(generics.RetrieveAPIView):
-    serializer_class = UserDetailSerializer
+    serializer_class = serializers.UserDetailSerializer
     queryset = User.objects.all()
 
 
 class UserDetailWithAllDataView(generics.RetrieveAPIView):
-    serializer_class = UserDetailWithAllData
+    serializer_class = serializers.UserDetailWithAllData
     queryset = User.objects.all()
-
-
-class AllCategoriesListView(generics.ListAPIView):
-    serializer_class = CategoriesListSerializer
-    queryset = Categories.objects.all()
 
 
 class UserDetailsCategoriesView(generics.RetrieveAPIView):
-    serializer_class = UserCategoryDetails
+    serializer_class = serializers.UserCategoryDetails
     queryset = User.objects.all()
 
+
+# Categories based views
+class AllCategoriesListView(generics.ListAPIView):
+    serializer_class = serializers.CategoriesListSerializer
+    queryset = Categories.objects.all()
+
+
+class CategoryDetailsWithTopicsView(generics.RetrieveAPIView):
+    serializer_class = serializers.CategoryDetailsWithTopicsOnlySerializer
+    queryset = Categories.objects.all()
+
+
+# Topics based views
+class AllTopicsListView(generics.ListAPIView):
+    serializer_class = serializers.TopicsListSerializer
+    queryset = Topics.objects.all()
+
+
+class TopicDetailsWithNotesView(generics.RetrieveAPIView):
+    serializer_class = serializers.TopicsListWithNotesSerializer
+    queryset = Topics.objects.all()
+
+
+# Notes based views
+class AllNotesListView(generics.ListAPIView):
+    serializer_class = serializers.NotesListSerializer
+    queryset = Notes.objects.all()
+
+
+class NoteDetailsView(generics.RetrieveAPIView):
+    serializer_class = serializers.NotesListSerializer
+    queryset = Notes.objects.all()
