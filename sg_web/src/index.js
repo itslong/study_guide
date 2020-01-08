457faefeefb3fcab6d611e649a35b83ad7f6edc8
Long Liang <lliang@Earth-C-137.local>
2020-01-08 12:50:29 -0800

    loads optional App based on url. Remove when react-router is added.

--- sg_web/src/index.js
+++ sg_web/src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import { App as Example } from './components/examples/App';
 import App from './components/App';
 import { exampleStore as store } from './js/store';
 
 
+// delete when react-router is implemented.
+const OptionalApp = window.location.pathname == '/example/' ? Example : App;
+
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <OptionalApp />
     </Provider>,
     document.getElementById('root')
 );
--- study_guide/urls.py
+++ study_guide/urls.py
@@ -20,6 +20,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('example/', views.example),
     path('admin/', admin.site.urls),
 ]
 
