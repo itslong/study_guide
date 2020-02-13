f912aaa5ed7124e16d3395850c6453bf25f24d95
Long Liang <lliang@Earth-C-137.local>
2020-02-13 00:50:43 -0800

    created LoginForm and updated auth endpoints.

--- sg_web/src/components/common/FormFields.js
+++ sg_web/src/components/common/FormFields.js
@@ -5,26 +5,26 @@ import { Input } from './';
 
 const FormFields = ({ fields, submitHandler=null }) => {
     const formFields = fields.map((field, index) => {
-      const { type, name, onChange, label, value } = field;
+        const { type, name, onChange, label, value } = field;
 
-      if (type === 'input') {
-          return (
-              <Input 
-                key={name} 
-                type={type} 
-                name={name} 
-                title={label} 
-                handleChange={onChange}
-                value={value}
-              />
-          )
-      }
+        if (type === 'input') {
+            return (
+                <Input 
+                  key={name} 
+                  type={type} 
+                  name={name} 
+                  title={label} 
+                  handleChange={onChange}
+                  value={value}
+                />
+            );
+        }
     });
 
     return (
-      <div>
-        {formFields}
-      </div>
+        <div>
+            {formFields}
+        </div>
     )
 
 }
